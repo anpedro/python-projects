eea1faece4afaf2d84ca87cbc585b129fe924aff
anpedro <87051194+anpedro@users.noreply.github.com>
2022-04-14 09:31:29 -0400

    Update ios_xr_napalm_interface_details.py

--- ios_xr_napalm_interface_details.py
+++ ios_xr_napalm_interface_details.py
@@ -29,7 +29,7 @@ def interface_errors():
 def main():
 
     xr_supported_models = ['get_bgp_config', 'get_bgp_neighbors', 'get_bgp_neighbors_detail', 'get_config', 'get_environment', 'get_facts', 'get_interfaces', 'get_interfaces_counters','get_interfaces_ip','get_lldp_neighbors','get_lldp_neighbors_detail','get_mac_address_table','get_ntp_peers','get_ntp_servers','get_ntp_stats','get_probes_config','get_probes_results','get_route_to','get_snmp_information','get_users','is_alive']
-    """Getting Interface non-zero counters/mac-address/mtu/speed on the entire A-MSW-Testbed"""
+    """Getting Interface non-zero counters/mac-address/mtu/speed"""
     list_of_hosts = ['10.8.70.12', '10.8.70.13', '10.8.70.14', '10.8.70.15', '10.8.70.22', '10.8.70.23', '10.8.70.25', '10.8.70.26']
 
     for host in list_of_hosts:
